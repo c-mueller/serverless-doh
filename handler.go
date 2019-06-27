58ba45d1eabaab9d1ce0fbd09370013d0445373e
Christian Müller <dev@c-mueller.xyz>
2019-06-27 02:14:33 +0200

    Move GCP Handler

--- handler.go
+++ handler.go
@@ -11,7 +11,7 @@ func HandleDNS(w http.ResponseWriter, r *http.Request) {
 	hndlr, _ := core.NewHandler(&core.Config{
 		EnableBlocking: true,
 		TCPOnly:        true,
-		Upstream:       []string{"1.1.1.1", "8.8.8.8"},
+		Upstream:       []string{"1.1.1.1:53","1.0.0.1:53", "8.8.8.8:53", "8.8.4.4:53"},
 		Verbose:        false,
 		UserAgent:      config.GetUserAgent(),
 		LogGuessedIP:   false,
