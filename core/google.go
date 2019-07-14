25d6994a669d0c82fe277f25989eee76832e3046
Christian Müller <dev@c-mueller.xyz>
2019-07-14 13:39:06 +0200

    Add Openwhisk Function Body

--- core/google.go
+++ core/google.go
@@ -1,6 +1,5 @@
 package core
 
-
 import (
 	"context"
 	"encoding/json"
--- lambda/lambda.go
+++ lambda/lambda.go
@@ -10,7 +10,7 @@ func main() {
 	hndlr, _ := core.NewHandler(&core.Config{
 		EnableBlocking: true,
 		TCPOnly:        true,
-		Upstream:       []string{"1.1.1.1", "8.8.8.8"},
+		Upstream:       []string{"1.1.1.1:53", "8.8.8.8:53"},
 		Verbose:        false,
 		UserAgent:      config.GetUserAgent(),
 		LogGuessedIP:   false,
